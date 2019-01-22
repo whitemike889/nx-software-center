5c4ad58eb710b0ff66ef091a76c9170e52b6ddbb
Alexis Lopez Zubieta <azubieta90@gmail.com>
2019-01-22 11:12:17 -0600

    Use LibAppImage instead of AppImageKit

--- cmake/libappimage.cmake
+++ cmake/libappimage.cmake
@@ -1,24 +1,24 @@
 message(STATUS "Downloading and building LibAppImage")
 
-find_package(AppImageKit QUIET)
+find_package(LibAppImage QUIET)
 
-if ( NOT AppImageKit_FOUND)
+if(NOT LibAppImage_FOUND)
     include(ExternalProject)
 
-    ExternalProject_Add(AppImageKit
-        GIT_REPOSITORY https://github.com/AppImage/AppImageKit.git
-        GIT_TAG appimagetool/master
-        GIT_SUBMODULES ""
+    ExternalProject_Add(LibAppImage_External
+        GIT_REPOSITORY https://github.com/AppImage/libappimage.git
+        GIT_TAG master
+        GIT_SHALLOW On
         CONFIGURE_COMMAND ${CMAKE_COMMAND} -G${CMAKE_GENERATOR} -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} <SOURCE_DIR>
         BUILD_COMMAND make gtest libappimage
         INSTALL_COMMAND make install DESTDIR=<INSTALL_DIR>
         )
 
-    ExternalProject_Get_Property(AppImageKit INSTALL_DIR)
-    set(AppImageKit_INSTALL_DIR ${INSTALL_DIR})
+    ExternalProject_Get_Property(LibAppImage_External INSTALL_DIR)
+    set(LibAppImage_INSTALL_DIR ${INSTALL_DIR})
 
-    set(libappimage_PATH ${AppImageKit_INSTALL_DIR}${CMAKE_INSTALL_PREFIX}/lib/libappimage.so)
-    set(libappimage_INCLUDE_DIRECTORIES ${AppImageKit_INSTALL_DIR}${CMAKE_INSTALL_PREFIX}/include/)
+    set(libappimage_PATH ${LibAppImage_INSTALL_DIR}${CMAKE_INSTALL_PREFIX}/lib/libappimage.so)
+    set(libappimage_INCLUDE_DIRECTORIES ${LibAppImage_INSTALL_DIR}${CMAKE_INSTALL_PREFIX}/include/)
 
 
     add_library(libappimage SHARED IMPORTED)
@@ -29,5 +29,5 @@ if ( NOT AppImageKit_FOUND)
         INTERFACE_LINK_LIBRARIES "glib-2.0"
         )
 
-    add_dependencies(libappimage AppImageKit)
-endif(NOT AppImageKit_FOUND)
+    add_dependencies(libappimage LibAppImage_External)
+endif(NOT LibAppImage_FOUND)
